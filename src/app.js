de9e0eed26cb0991627e3467bdb89c30e77c84ab
Joel Mann <jmann0115@hotmail.com>
2018-11-04 12:29:36 -0600

    remove dummy data from main app.js file

--- src/app.js
+++ src/app.js
@@ -6,9 +6,6 @@ import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import getVisibleExpenses from './selectors/expenses';
-import {addExpense} from "./actions/expenses";
-import {setTextFilter} from "./actions/filters";
 
 const store = configureStore();
 
@@ -17,9 +14,5 @@ const jsx = (
         <AppRouter/>
     </Provider>
 );
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 1200, createdAt: 123 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 5500, createdAt: 23 }));
-store.dispatch(addExpense({ description: 'Water Bill', amount: 109500, createdAt: -9 }));
 
 ReactDOM.render(jsx, document.getElementById('app'));
